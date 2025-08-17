026e8250a04c69b2d921fdb3bde0982bfbd636e4
Harshit <harshitmalik29@gmail.com>
2025-08-17 22:35:52 +0530

    removed files and created fresh ones

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
 
-import './App.css'
-import { Button } from './components/ui/Button'
-import { PlusIcon } from './icons/PlusIcon'
-import { ShareIcon } from './icons/shareIcon'
-
 function App() {
-  return (
-    <div className="p-8">
-        <div className="flex flex-row gap-4">
-            <Button startIcon={<PlusIcon size="lg" />} endIcon={<ShareIcon size="lg" />} variant="primary" size="sm" text="Share" onClick={() => console.log('Share clicked')} />
-            <Button variant="secondary" size="md" text="Add Content" onClick={() => console.log('Add Content clicked')} />
-        </div>
-    </div>
-  )
+  return <div className="bg-red-300">
+    hi there
+  </div>
 }
 
 export default App
+ 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,7 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        purple: {
-          300: "#e0e7fe",
-          500: "#3e38a7",
-          600: "#5046e4"
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
