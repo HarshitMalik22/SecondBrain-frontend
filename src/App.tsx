d5a746af9b0d99e902bb60a8272f7a66dec7cfbb
Harshit <harshitmalik29@gmail.com>
2025-08-18 00:15:19 +0530

    now fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Button } from "./components/button"
+import { Button } from "./components/Button"
 import { PlusIcon } from "./icons/PlusIcon"
 import { ShareIcon } from "./icons/ShareIcon"
 
