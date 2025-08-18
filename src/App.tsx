a062cc5431eec57706abd1c1a1794d1a6fca75a4
Harshit <harshitmalik29@gmail.com>
2025-08-18 22:54:08 +0530

    started adding card components(done till yt link)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Button } from "./components/Button"
 import { PlusIcon } from "./icons/PlusIcon"
 import { ShareIcon } from "./icons/ShareIcon"
+import { Card } from "./components/Card"
 
 function App() {
   return <div>
     <Button variant="primary" text="add content" startIcon={<PlusIcon/>}></Button> 
     <Button variant="secondary" text="Share Brain" startIcon={<ShareIcon/>}></Button>
+
+    <Card/>
   </div>
 }
 
