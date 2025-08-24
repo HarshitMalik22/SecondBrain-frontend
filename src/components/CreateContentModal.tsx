7fa52a92c0fc59423c37352be56ad39edca2a6ff
Harshit <harshitmalik29@gmail.com>
2025-08-24 21:32:31 +0530

    added CrossIcon

--- src/components/CreateContentModal.tsx
+++ src/components/CreateContentModal.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
+import { CrossIcon } from "../icons/CrossIcon";
 
 export function CreateContentModal({open, onClose}){
     return <div>
-        {open && <div className="w-screen h-screen bg-red-200 fixed top-0 left-0 opacity-60 flex justify-center">
+        {open && <div className="w-screen h-screen bg-slate-200 fixed top-0 left-0 opacity-60 flex justify-center">
             <div className="flex flex-col justify-center">
-                <span className="bg-white-opacity-100">
-                    hi there
+                <span className="bg-white opacity-100 p-4 rounded-lg">
+                    <div className="flex justify-end">
+                        <CrossIcon />
+                    </div>
                 </span>
             </div>
         </div>}
