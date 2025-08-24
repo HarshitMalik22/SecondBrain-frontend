8fcb8cb1dd94e8140990bf3aa501c5d482d40dec
Harshit <harshitmalik29@gmail.com>
2025-08-24 21:49:46 +0530

    adding submit button

--- src/components/CreateContentModal.tsx
+++ src/components/CreateContentModal.tsx
@@ -9,8 +9,21 @@ export function CreateContentModal({open, onClose}){
                     <div className="flex justify-end">
                         <CrossIcon />
                     </div>
+                    <div>
+                        <Input placeholder="Title" />
+                        <Input placeholder="Link" />
+                    </div>
+                    <div className="flex justify-center">
+                        <Button variant="primary" text="Submit" />
+                    </div>
                 </span>
             </div>
         </div>}
+    </div>
+}
+
+function Input ({onChange, placeholder}: {onChange: (e: React.ChangeEvent<HTMLInputElement>) => void, placeholder: string }) {
+    return <div>
+        <input placeholder = {placeholder} type={"text"} className="px-4 py-2 border rounded" onChange={onChange}></input>
     </div>
 }
