98c7d9afbb26d682bf6fe450fd6f8b9b92aca902
Harshit <harshitmalik29@gmail.com>
2025-08-25 12:45:50 +0530

    twitter, yt icons fixed working

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,15 @@
+import { TwitterIcon } from "../icons/TwitterIcon";
+import { YoutubeIcon } from "../icons/YoutubeIcon";
+import { SidebarItem } from "./SidebarItem";
+
 export function Sidebar (){
     return <div className="h-screen bg-whit border-r w-76 fixed left-0 top-0">
-        hi there
+        <h1>
+            SecondBrain
+        </h1>
+        <div className="pt-4 pl-4">
+            <SidebarItem text="Twitter" icon={<TwitterIcon />} />
+            <SidebarItem text="Youtube" icon={<YoutubeIcon />} />
+        </div>
     </div>
-}
+}  
--- src/components/SidebarItem.tsx
+++ src/components/SidebarItem.tsx
@@ -5,8 +5,12 @@ export function SidebarItem({text, icon}:{
     icon: ReactElement;
 }
 ){
-    return <div className="flex">
-        {icon}
-        {text}
+    return <div className="flex text-gray-800 ">
+        <div className="p-2">
+            {icon}
+        </div>
+        <div className="p-2">
+            {text}
+        </div>
     </div>
 }
